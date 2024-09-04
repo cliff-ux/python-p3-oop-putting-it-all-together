244e48fa63e364385b7be0c819104073bf579c55
cliff <rydleecliff@gmail.com>
2024-09-04 08:53:09 +0300

    lab1

---
+++ booktest.py
@@ -0,0 +1,13 @@
+@property
+def page_count(self):
+    return self._page_count
+
+@page_count.setter
+def page_count(self, value):
+    if isinstance(value, int):
+        self._page_count = value
+    else:
+        print("page_count must be an integer")
+
+def turn_page(self):
+    print("Flipping the page...wow, you read fast!")
